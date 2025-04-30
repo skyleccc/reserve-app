3d1c265fa5a1ab07a2bb78d245168d0d0fcc860a
Munckenh <76769232+Munckenh@users.noreply.github.com>
2025-04-30 09:33:05 +0800

    add: add location activity

---
+++ app/src/main/java/com/reserve/app/AddLocation.java
@@ -0,0 +1,51 @@
+package com.reserve.app;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class AddLocation extends AppCompatActivity {
+    TextView tvHeader, tvExplore, tvSaved, tvUpdates, tvAdd;
+    ShapeableImageView ivProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_location);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize views
+        tvHeader = findViewById(R.id.tv_header);
+        ivProfile = findViewById(R.id.iv_profile);
+        tvExplore = findViewById(R.id.tv_explore);
+        tvSaved = findViewById(R.id.tv_saved);
+        tvUpdates = findViewById(R.id.tv_updates);
+        tvAdd = findViewById(R.id.tv_add);
+
+        // Load custom fonts
+        Typeface lexendExaSemiFont = Typeface.createFromAsset(getAssets(), "fonts/LexendExa/LexendExa-SemiBold.ttf");
+        Typeface interRegularFont = Typeface.createFromAsset(getAssets(), "fonts/Inter/Inter_18pt-Regular.ttf");
+        Typeface interMediumFont = Typeface.createFromAsset(getAssets(), "fonts/Inter/Inter_18pt-Medium.ttf");
+
+
+        // Set custom font
+        tvHeader.setTypeface(lexendExaSemiFont);
+        tvExplore.setTypeface(interMediumFont);
+        tvSaved.setTypeface(interMediumFont);
+        tvUpdates.setTypeface(interMediumFont);
+        tvAdd.setTypeface(interMediumFont);
+    }
+}
